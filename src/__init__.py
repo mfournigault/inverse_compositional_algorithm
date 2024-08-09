f3cfc64a0ab19e0a3d5ba116f07f7be69ca4fda9
mfournigault <mike.fournigault1@gmail.com>
2024-08-09 17:31:52 +0200

    Renamed the module InverseCompositionAlgorithm to match names of other modules \nChanged default values of algorithm meta parameters to better values in the config handler \nFixed some small bugs \n TODO Finish the algorithm validations in the test notebook

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+__all__ = ["bicubic_interpolation", 
+    "configuratin_handler", 
+    "constants",
+    "derivatives",
+    "image_optimisation",
+    "_inverse_compositional_algorithm",
+    "matrix_operators",
+    "transformation",
+    "zoom"]
