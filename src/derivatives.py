inverse_compositional_algorithm function: Corrected multiple bugs due to the mix of non-flat and flat images and matrices -> to be validated\nAdded some tests for derivatives functions

src/derivatives.py
@@ -20,7 +20,7 @@ def jacobian(transform_type, nx, ny):
     """
     nparams = transform_type.nparams()
 
-    J = np.zeros((2 * nx * ny, nparams))
+    J = np.zeros((2 * nx * ny * nparams))
 
     match transform_type:
         case TransformType.TRANSLATION:
@@ -110,8 +110,10 @@ def hessian(DIJ):
     # Calculate the Hessian in a neighbor window
     for i in range(ny):
         for j in range(nx):
-            DIJ_slice = DIJ[(i * nx + j) * nz * nparams : (i * nx + j + 1) * nz * nparams]
-            H += AtA(DIJ_slice, nz, nparams)
+            # DIJ_slice = DIJ[(i * nx + j) * nz * nparams : (i * nx + j + 1) * nz * nparams]
+            DIJ_slice = DIJ[i, i, :, :]
+            # H += AtA(DIJ_slice, nz, nparams)
+            H += DIJ_slice.T @ DIJ_slice
     
     return H
 

src/image_optimisation.py
@@ -94,12 +94,13 @@ def independent_vector(DIJ, DI, nparams, nx, ny, nz):
 
     for i in range(ny):
         for j in range(nx):
-            # DIJ, DI are supposed to be flattened
-            b += Atb(
-                DIJ[(i * nx + j) * nparams * nz : (i * nx + j + 1) * nparams * nz],
-                DI[(i * nx + j) * nz : (i * nx + j + 1) * nz],
-                nz, nparams
-            )
+            # DIJ, DI are supposed to not be flattened
+            # b += Atb(
+            #     DIJ[(i * nx + j) * nparams * nz : (i * nx + j + 1) * nparams * nz],
+            #     DI[(i * nx + j) * nz : (i * nx + j + 1) * nz],
+            #     nz, nparams
+            # )
+            b += np.dot(DIJ[j, i, :, :].T, DI[j, i, :])
             # b is flattened
 
     return b
@@ -140,7 +141,7 @@ def independent_vector_robust(DIJ, DI, rho, nparams, nx, ny, nz):
 
 def parametric_solve(H_1, b, nparams):
     # Convert inputs to numpy arrays
-    H_1 = np.array(H_1).reshape((nparams, nparams))
+    # H_1 = np.array(H_1).reshape((nparams, nparams))
     b = np.array(b)
     
     # Perform matrix-vector multiplication
@@ -160,18 +161,18 @@ def steepest_descent_images(Ix, Iy, J, nparams):
         Ix (ndarray): The x-derivative of the input image.
         Iy (ndarray): The y-derivative of the input image.
         J (ndarray): The Jacobian matrix.
-        nparams (int): The number of parameters.
-        nx (int): The number of columns in the image.
-        ny (int): The number of rows in the image.
-        nz (int): The number of channels in the image.
+        nparams (int): The number of parameters of the transformation.
 
     Returns:
         ndarray: The steepest descent images.
 
     """
-   #TODO: remove params nx, ny, nz and define them from the shape of Ix, Iy and J
-   #TODO: implement sanity check on the dimensions of Ix, Iy and J, they should be compatible 
-    ny, nx, nz = Ix.shape
+    # Define nx, ny, nz from the shape of Ix 
+    ny, nx, nz = Ix.shape # suppose that Ix and Iy are not flattened
+
+    # Verify the dimensions of Ix and Iy
+    if Ix.shape != Iy.shape:
+        raise ValueError("Ix and Iy must have the same dimensions")
 
 
     # Initialize the output array
@@ -183,10 +184,10 @@ def steepest_descent_images(Ix, Iy, J, nparams):
             for c in range(nz):
                 p = i * nx + j
                 for n in range(nparams):
-                    DIJ[k] = Ix[i, j, c] * J[2 * p, n] + Iy[i, j, c] * J[2 * p + 1, n]
+                    # DIJ[k++]=Ix[p*nz+c]*J[2*p*nparams+n]+Iy[p*nz+c]*J[2*p*nparams+n+nparams];
+                    DIJ[k] = Ix[i, j, c] * J[i, j, n] + Iy[i, j, c] * J[i, j, n + nparams]
                     k += 1
     
-    #TODO reshape DIJ to not be flattened
     DIJ = DIJ.reshape((ny, nx, nz, nparams))
 
     return DIJ