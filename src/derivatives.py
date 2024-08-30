In progress: optimisation of hessian, independant_vector computations. Algorithm validated on the 1st iteration for a translation. TODO: correct the masking between DIJ and DI in independant_vector to ensure that we work on the same domain for both "images)

src/derivatives.py
@@ -1,8 +1,8 @@
 import numpy as np
 from matrix_operators import AtA, sAtA
+from numba import jit
 from transformation import TransformType
 import utils
-
 
 def jacobian(transform_type, nx, ny):
     """
@@ -66,7 +66,7 @@ def jacobian(transform_type, nx, ny):
 
     return J
 
-    
+
 def hessian(DIJ):
     """
     Function to compute the Hessian matrix.
@@ -83,18 +83,47 @@ def hessian(DIJ):
     #         DIJ_slice = DIJ[i, j, :, :]
     #         if utils.valid_values(DIJ_slice):
     #             H += DIJ_slice.T @ DIJ_slice
+    DIJ_reshaped = DIJ.reshape(ny * nx, nz * nparams)
+    print("DIJ reshaped: ", DIJ_reshaped.shape)
+
+    # Create a boolean mask for valid values (excluding NaN and Inf)
+    valid_mask = np.isfinite(DIJ_reshaped)
+    print("valid mask: ", valid_mask.shape)
+     # Check if there are any valid values
+    if not np.any(valid_mask):
+        print("Warning: No valid values found in DIJ.")
+        raise ValueError("No valid values found in DIJ.")  # Or return a default value
+    
+    # Filter DIJ_reshaped based on the valid mask
+    DIJ_reshaped_valid = DIJ_reshaped[valid_mask]
+    # Reshape back to 2D if necessary (based on your specific requirements)
+    if DIJ_reshaped_valid.ndim == 1:
+        DIJ_reshaped_valid = DIJ_reshaped_valid.reshape(-1, nparams)
+    print("valid DIJ: ", DIJ_reshaped_valid.shape)
+    
+    # Calculate the Hessian using matrix multiplication on valid values
+    H = DIJ_reshaped_valid.T @ DIJ_reshaped_valid
+    print("hessian shape: ", H.shape)
+
     # Filter valid slices
     # valid_slices = np.array([utils.valid_values(DIJ[i, j, :, :]) for i in range(ny) for j in range(nx)])
     # valid_slices = valid_slices.reshape(ny, nx)
-    mask_DIJ = ~np.isnan(DIJ) & ~np.isinf(DIJ)
+    # mask_DIJ = np.ones((ny, nx, nz), dtype=bool)
+    # for n in range(nparams):
+    #     mask_DIJ &= ~np.isnan(DIJ[:,:,:,n]) & ~np.isinf(DIJ[:,:,:,n])
+    # print("Derivatives mask: ", mask_DIJ.shape)
     
-    # Extract valid slices of the steepest descent images
-    valid_DIJs = np.where(mask_DIJ, DIJ, 0) # replacing invalid values with 0 cannot work for the Hessian
+    # # Extract valid slices of the steepest descent images
+    # valid_DIJs = np.zeros((ny, nx, nz, nparams), dtype=np.float64)
+    # valid_DIJs[mask_DIJ] = DIJ[mask_DIJ]
     
-    # Compute the Hessian matrix with vectorized operations
-    H = np.einsum('...ij,...kj->...ik', valid_DIJs, valid_DIJs).sum(axis=0)
+    # # Compute the Hessian matrix with vectorized operations
+    # temp = np.einsum('...ji,...ik->...jk', valid_DIJs, valid_DIJs)
+    # print("hessian product: ", temp.shape)
+    # H = temp
     
     return H
+
 
 def hessian_robust(DIJ, rho, nparams):
     """

src/image_optimisation.py
@@ -1,5 +1,7 @@
 import numpy as np
 from enum import Enum
+from numba import jit
+
 from matrix_operators import AtA, sAtA, Atb, sAtb
 import utils
 
@@ -90,7 +92,6 @@ def robust_error_function(DI, lambda_, type_):
     return rho
 
 
-
 def independent_vector(DIJ, DI, nparams):
     """
     Function to compute b=Sum(DIJ^t * DI)
@@ -115,20 +116,42 @@ def independent_vector(DIJ, DI, nparams):
     #         except IndexError:
     #             print(f"IndexError: i={i}, j={j}, DIJ.shape={DIJ.shape}, DI.shape={DI.shape}")
     #             raise
+    # Reshape DIJ and DI to 2D matrices
+    DIJ_reshaped = DIJ.reshape(ny * nx, nz * nparams)
+    DI_reshaped = DI.reshape(ny * nx, nz)
+    
+    # Create a boolean mask for valid values
+    valid_mask_dij = np.isfinite(DIJ_reshaped)
+    valid_mask_di = np.isfinite(DI_reshaped)    
+    
+    # Filter DIJ_reshaped and DI_reshaped based on the valid mask
+    DIJ_reshaped_valid = DIJ_reshaped[valid_mask_dij]
+    if DIJ_reshaped_valid.ndim == 1:
+        DIJ_reshaped_valid = DIJ_reshaped_valid.reshape(-1, nparams)
+    DI_reshaped_valid = DI_reshaped[valid_mask_di]
+    
+    # Calculate b using matrix multiplication
+    b = DIJ_reshaped_valid.T @ DI_reshaped_valid
+    print("b shape: ", b.shape)
     
     # Create a mask for valid values
     # valid_mask = np.all(utils.valid_values(DIJ), axis=(2, 3)) & np.all(utils.valid_values(DI), axis=2)
     # valid_mask = np.all(utils.valid_values(DIJ)) & np.all(utils.valid_values(DI))
 
     # Filter valid values
-    mask_DIJ = ~np.isnan(DIJ) & ~np.isinf(DIJ)
-    mask_DI = ~np.isnan(DI) & ~np.isinf(DI)
+    # mask_DIJ = np.ones((ny, nx, nz), dtype=bool)
+    # for n in range(nparams):
+    #     mask_DIJ &= ~np.isnan(DIJ[:,:,:,n]) & ~np.isinf(DIJ[:,:,:,n]) &~np.isnan(DI) & ~np.isinf(DI)
+    # print("Indep. vector mask_DIJ", mask_DIJ.shape)
+    # mask_DI = ~np.isnan(DI) & ~np.isinf(DI)
 
-    filtered_DIJ = np.where(mask_DIJ, DIJ, 0) # replacing invalid values with 0 cannot work for the independent vector
-    filtered_DI = np.where(mask_DI, DI, 0) # replacing invalid values with 0 cannot work for the independent vector
+    # valid_DIJs = np.zeros((ny, nx, nz, nparams), dtype=np.float64)
+    # valid_DIJs[mask_DIJ] = DIJ[mask_DIJ]
+    # valid_DI = np.zeros((ny, nx, nz), dtype=np.float64)
+    # valid_DI[mask_DIJ] = DI[mask_DIJ]
 
-    # Effectuer la multiplication en ignorant les valeurs NaN et Inf
-    result = np.einsum('...ij,...i->...j', filtered_DIJ, filtered_DI).sum(axis=0)
+    # # Effectuer la multiplication en ignorant les valeurs NaN et Inf
+    # b = np.einsum('...ij,...i->...j', valid_DIJs, valid_DI).sum(axis=0)
 
     return b
 
@@ -185,6 +208,7 @@ def parametric_solve(H_1, b, nparams):
     return np.sqrt(error), dp
 
 
+@jit(nopython=True, fastmath=True, nogil=True, cache=True, parallel=True)
 def steepest_descent_images(Ix, Iy, J, nparams):
     """
     Calculate the steepest descent images DI^t*J for optimization.