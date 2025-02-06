RobustInverseCompositional : debug in progress, calculation of H and b to be fixed

src/inverse_compositional_algorithm_keras.py
@@ -19,48 +19,54 @@ def cubic(x):
                              (-0.5 * absx3 + 2.5 * absx2 - 4.0 * absx + 2.0),
                              tf.zeros_like(x)))
 
+
 def get_pixel_value(img, x, y):
     # img : [batch, H, W, C]
-    # x, y : coordonnées entières, mêmes dimensions [batch, ny, nx]
-    batch_size = tf.shape(img)[0]
-    batch_idx = tf.reshape(tf.range(batch_size), [batch_size, 1, 1])
-    b = tf.tile(batch_idx, [1, tf.shape(x)[1], tf.shape(x)[2]]) #TODO: debug this
-    indices = tf.stack([b, y, x], axis=-1)
+    xt = tf.squeeze(x)
+    yt = tf.squeeze(y)
+
+    batch_size = tf.shape(img)[0]  
+    batch_idx = tf.range(batch_size)                        # (batch_size,)
+    B, Y, X = tf.meshgrid(batch_idx, yt, xt, indexing="ij")
+    indices = tf.stack([B, Y, X], axis=-1)  # (batch, ny, nx, channels)
+    
     return tf.gather_nd(img, indices)
+
 
 def bicubic_sampler(image, grid):
     # image: [batch, H, W, C]
     # grid: [batch, newH, newW, 2] avec coordonnées (y, x) en flottants
     input_shape = tf.shape(image)
     batch_size, H, W, channels = input_shape[0], input_shape[1], input_shape[2], input_shape[3]
-
-    grid_y = grid[..., 0]
-    grid_x = grid[..., 1]
+    grid_y = grid[:, :, 0]  # extract y coordinates with shape [batch, H]
+    grid_x = grid[:, 0, :]  # extract x coordinates with shape [batch, W]
 
     # Calcul des coordonnées entières pour le voisinage (début)
-    x0 = tf.cast(tf.floor(grid_x), tf.int32)
-    y0 = tf.cast(tf.floor(grid_y), tf.int32)
+    x0 = tf.cast(tf.floor(grid_x), tf.int32)  # shape [batch, W]
+    y0 = tf.cast(tf.floor(grid_y), tf.int32)  # shape [batch, H]
 
     # Pour l'interpolation bicubique, on a besoin de 4 pixels dans chaque direction.
     # On crée donc les 4 indices autour de (x0,y0)
-    x_indexes = [x0 - 1, x0, x0 + 1, x0 + 2]
-    y_indexes = [y0 - 1, y0, y0 + 1, y0 + 2]
+    x_indexes = [x0 - 1, x0, x0 + 1, x0 + 2]  # list of 4 indices with shape [batch, W]
+    y_indexes = [y0 - 1, y0, y0 + 1, y0 + 2]  # list of 4 indices with shape [batch, H]
 
     # Calcul des poids selon la distance
-    x_diff = grid_x - tf.cast(x0, tf.float32)
-    y_diff = grid_y - tf.cast(y0, tf.float32)
+    x_diff = grid_x - tf.cast(x0, tf.float32)  # shape [batch, W]
+    y_diff = grid_y - tf.cast(y0, tf.float32)  # shape [batch, H]
 
+    # list of 4 indices with shape [batch, W]
     weights_x = [cubic(x_diff + 1.0), cubic(x_diff), cubic(x_diff - 1.0), cubic(x_diff - 2.0)]
+    # list of 4 indices with shape [batch, H]
     weights_y = [cubic(y_diff + 1.0), cubic(y_diff), cubic(y_diff - 1.0), cubic(y_diff - 2.0)]
 
-    # Convertir les listes en tenseur de forme [batch, newH, newW, 4]
-    weights_x = tf.stack(weights_x, axis=-1)
-    weights_y = tf.stack(weights_y, axis=-1)
+    # Convert list into tensors with shape [batch, d, 4]
+    weights_x = tf.stack(weights_x, axis=-1)  # list of 4 tensors with shape [batch, W] -> [batch, W, 4]
+    weights_y = tf.stack(weights_y, axis=-1)  # list of 4 tensors with shape [batch, H] -> [batch, H, 4]
 
-    # Initialisation du résultat
+    # Tensor with shape [batch, H, W, channels]
     output = tf.zeros([batch_size, tf.shape(grid)[1], tf.shape(grid)[2], channels], dtype=image.dtype)
 
-    # Accumulation sur les 16 voisins
+    # Accumulation on the 16 neighbors
     for i in range(4):
         for j in range(4):
             # Sélection des indices i, j
@@ -70,9 +76,8 @@ def bicubic_sampler(image, grid):
             x_i = tf.clip_by_value(x_i, 0, W - 1)
             y_j = tf.clip_by_value(y_j, 0, H - 1)
             pixel = get_pixel_value(image, x_i, y_j)  # [batch, newH, newW, channels]
-            w = weights_x[..., i] * weights_y[..., j]   # [batch, newH, newW]
-            w = tf.expand_dims(w, axis=-1)
-            output += pixel * w
+            w = tf.expand_dims(weights_y[:, :, j], axis=-1) * tf.expand_dims(weights_x[:, :, i], axis=1)
+            output += pixel * tf.expand_dims(w, axis=-1)  # expand_dims of w to replicate it for all channels
     return output
 
 class RobustInverseCompositional(Layer):
@@ -129,33 +134,43 @@ class RobustInverseCompositional(Layer):
                 J = tf.stack([ones, zeros, X, Y, zeros, zeros, zeros, ones, zeros, zeros, X, Y], axis=-1)
             case tr.TransformationType.HOMOGRAPHY:
                 J = tf.stack([X, Y, ones, zeros, zeros, zeros, -X*X, -X*Y, zeros, zeros, zeros, X, Y, ones, -X*Y, -Y*Y], axis=-1)
-        
-        print("shape of J: ", J.shape)
-        J = J[..., :self.nparams]
-        print("shape of J: ", J.shape)
+        print("--- J shape: ", J.shape)
+        # J = J[..., :self.nparams]
+        # print("----- J shape: ", J.shape)
+        #TODO: fix bug J shape should be (ny, nx, 2*nparams) and not (ny, nx, nparams)
         return tf.expand_dims(J, 0)  # Add batch dimension
 
     def steepest_descent_images(self, Ix, Iy, J):
         # gradients = tf.stack([Ix, Iy], axis=-1)
         # return tf.einsum('bhwci,bhwji->bhwj', gradients, J) #TODO: Check if this is correct
         # Supposons que J a une taille (b, ny, nx, 2*m) avec m = nparams/2
+        print("J shape: ", J.shape)
         Jx, Jy = tf.split(J, num_or_size_splits=2, axis=-1)  # Chaque tenseur a la taille (b, ny, nx, m)
+        print("Jx shape: ", Jx.shape)
+        print("Jy shape: ", Jy.shape)
 
         # Étendre Ix et Iy pour pouvoir multiplier par broadcast avec Jx et Jy.
         # Ix et Iy ont la taille (b, ny, nx, nz). On les étend sur la dernière dimension.
         Ix_exp = tf.expand_dims(Ix, axis=-1)  # (b, ny, nx, nz, 1)
         Iy_exp = tf.expand_dims(Iy, axis=-1)  # (b, ny, nx, nz, 1)
+        print("Ix_exp shape: ", Ix_exp.shape)
+        print("Iy_exp shape: ", Iy_exp.shape)
 
         # Reshape Jx et Jy pour qu'ils aient un axe "duplicate" pour nz.
         Jx_exp = tf.expand_dims(Jx, axis=2)  # (b, ny, 1, nx, m) ou réarranger selon l'ordre souhaité
         Jy_exp = tf.expand_dims(Jy, axis=2)  # (b, ny, 1, nx, m)
+        print("Jx_exp shape: ", Jx_exp.shape)
+        print("Jy_exp shape: ", Jy_exp.shape)
 
         # Pour correspondre aux dimensions, on peut réarranger Jx et Jy en (b, ny, nx, 1, m)
         Jx_exp = tf.reshape(Jx, [tf.shape(Jx)[0], tf.shape(Jx)[1], tf.shape(Jx)[2], 1, tf.shape(Jx)[3]])
         Jy_exp = tf.reshape(Jy, [tf.shape(Jy)[0], tf.shape(Jy)[1], tf.shape(Jy)[2], 1, tf.shape(Jy)[3]])
+        print("Jx_exp shape: ", Jx_exp.shape)
+        print("Jy_exp shape: ", Jy_exp.shape)
 
         # Alors, DIJ est défini par la somme des deux contributions :
         DIJ = Ix_exp * Jx_exp + Iy_exp * Jy_exp  # Résultat de taille (b, ny, nx, nz, m)
+        print("DIJ shape: ", DIJ.shape)
         return DIJ
 
     def warp_image(self, I2, p):
@@ -169,11 +184,21 @@ class RobustInverseCompositional(Layer):
             # Pour le masque, on utilise une interpolation en nearest neighbor.
             # Ici, nous pouvons utiliser tf.gather_nd sur les indices entiers.
             grid_int = tf.cast(tf.round(grid), tf.int32)
+        
             def sample_mask(mask, grid_int):
-                b = tf.reshape(tf.range(tf.shape(mask)[0]), [tf.shape(mask)[0], 1, 1])
-                b = tf.tile(b, [1, tf.shape(grid_int)[1], tf.shape(grid_int)[2]])
-                indices = tf.stack([b, grid_int[..., 0], grid_int[..., 1]], axis=-1)
+                # Supposons que mask a la forme (batch, H, W, channels)
+                # Créer une grille d'indices spatiaux (B, Y, X) de forme (batch, H, W)
+                B, Y, X, C = tf.meshgrid(tf.range(tf.shape(mask)[0]), 
+                                    tf.range(tf.shape(mask)[1]), 
+                                    tf.range(tf.shape(mask)[2]), 
+                                    tf.range(tf.shape(mask)[3]),
+                                    indexing="ij")
+
+                # Empiler pour obtenir des indices de forme (batch, H, W, channels, 4)
+                indices = tf.stack([B, Y, X, C], axis=-1)
+                # indices = tf.stack([B, Y, X], axis=-1)
                 return tf.gather_nd(mask, indices)
+
             mask_warped = sample_mask(mask, grid_int)
             warped = tf.where(mask_warped < 0.5,
                                 tf.constant(float('nan'), dtype=I2.dtype),
@@ -188,27 +213,17 @@ class RobustInverseCompositional(Layer):
         X, Y = tf.meshgrid(x, y)
         ones = tf.ones_like(X)
         coords = tf.stack([X, Y, ones], axis=0)
-        print("coords shape: ", coords.shape)
-        print("p shape: ", p.shape)
-        # affine_matrix = tf.reshape(p, [-1, 2, 3])
         # Use of map_fn to apply the function params2matrix to each element of p (batch of parameters)
         affine_matrix = tf.map_fn(lambda params: tr.params2matrix(params, self.transform_type), p, dtype=tf.float32)
-        print("shape of affine_matrix: ", affine_matrix.shape)
         transformed = tf.einsum('bij,jhw->bhw', affine_matrix, coords)
-        print("shape of transformed: ", transformed.shape)
-        # X_new = transformed[:, 0, :, :]
-        # Y_new = transformed[:, 1, :, :]
         
-        # # On construit la grille de coordonnées sous forme absolue en (y, x)
-        # grid = tf.stack([Y_new, X_new], axis=-1)
-        # return grid
         return transformed
 
     def robust_error(self, DI):
         # apply our robust error function, considering that in input we may have a batch of images
         rho_list = []
-        for image in self.batch_size:
-            rho = io.robust_error_function(DI, self.lambda_, self.robust_type)
+        for image in tf.range(self.batch_size):
+            rho = io.robust_error_function(DI.numpy()[image, :, :, :], self.lambda_, self.robust_type)
             # adding the robust error to the tensor of robust errors
             rho_list.append(rho)
         rho_tensor = tf.stack(rho_list, axis=0)
@@ -227,6 +242,8 @@ class RobustInverseCompositional(Layer):
         print("Iy shape: ", Iy.shape)
         DIJ = self.steepest_descent_images(Ix, Iy, J)
         print("DIJ shape: ", DIJ.shape)
+        # DIJ = tf.squeeze(DIJ, axis=4)
+        # print("DIJ shape: ", DIJ.shape)
         print("p shape: ", p.shape)
         
         def body(i, p, error):
@@ -236,9 +253,14 @@ class RobustInverseCompositional(Layer):
             
             # Compute robust error
             rho = self.robust_error(DI)
+            print("rho shape: ", rho.shape)
+            # rho = tf.squeeze(rho, axis=2)
+            # print("rho shape: ", rho.shape)
             
             # Compute Hessian and b
             weighted_DIJ = DIJ * tf.expand_dims(rho, -1)
+            DIJt = tf.einsum("bhwcn->bhwnc", DIJ)
+            #TODO: fix the calculation of H and b by following the numpy implementation
             H = tf.einsum('bhwij,bhwik->bjk', DIJ, weighted_DIJ)
             b = tf.einsum('bhwij,bhw->bj', DIJ, DI * rho)
             